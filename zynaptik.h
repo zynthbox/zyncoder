8bd1110875975b632f98f775eee60c5522bd9bce
Marco Martin <notmart@gmail.com>
2022-06-01 14:17:17 +0100

    port more files

--- zynaptik.h
+++ zynaptik.h
@@ -24,6 +24,9 @@
  * ******************************************************************
  */
 
+#include <jack/jack.h>
+#include <jack/midiport.h>
+
 //-----------------------------------------------------------------------------
 // MCP23017 Stuff
 //-----------------------------------------------------------------------------
@@ -54,42 +57,107 @@
 	#define ZYNAPTIK_ADS1115_BASE_PIN 300
 #endif
 
-#define ADS115_GAIN_VREF_6_144 0
-#define ADS115_GAIN_VREF_4_096 1
-#define ADS115_GAIN_VREF_2_048 2
-#define ADS115_GAIN_VREF_1_024 3
-#define ADS115_GAIN_VREF_0_512 4
-#define ADS115_GAIN_VREF_0_256 5
-
-void init_ads1115(uint16_t base_pin, uint16_t i2c_address);
-void set_ads1115_gain(uint16_t base_pin, uint8_t gain);
-
 //-----------------------------------------------------------------------------
-// CV-IN: Generate MIDI CC from Analog Inputs
+// MCP4728 Stuff
 //-----------------------------------------------------------------------------
 
-#define MAX_NUM_ZYNCVINS 8
+//Default config for Zynaptik's MCP4728
+#if !defined(ZYNAPTIK_MCP4728_I2C_ADDRESS)
+	#if ZYNAPTIK_VERSION==1
+		#define ZYNAPTIK_MCP4728_I2C_ADDRESS 0x60
+	#else
+		#define ZYNAPTIK_MCP4728_I2C_ADDRESS 0x61
+	#endif
+#endif
+
+void init_mcp4728(uint16_t i2c_address);
+
+//-----------------------------------------------------------------------------
+// CV-IN: Generate MIDI from Analog Inputs: CC, Pitchbend, Channel Pressure
+//-----------------------------------------------------------------------------
+
+#define MAX_NUM_ZYNCVINS 4
+#define DEFAULT_K_CVIN 1.03
 
 struct zyncvin_st {
 	uint8_t enabled;
 	uint16_t pin;
-	uint16_t val;
 
-	uint8_t midi_evt;
+	int midi_evt;
 	uint8_t midi_chan;
 	uint8_t midi_num;
-	uint8_t midi_val;
+	uint16_t midi_val;
 };
 struct zyncvin_st zyncvins[MAX_NUM_ZYNCVINS];
 
-void setup_zynaptik_cvin(uint8_t i, uint8_t midi_evt, uint8_t midi_chan, uint8_t midi_num);
+float k_cvin;
+void set_k_cvin(float k);
+float get_k_cvin();
+
+void setup_zynaptik_cvin(uint8_t i, int midi_evt, uint8_t midi_chan, uint8_t midi_num);
 void disable_zynaptik_cvin(uint8_t i);
-void send_zynaptik_cvin_midi(uint8_t i);
+void zynaptik_cvin_to_midi(uint8_t i, uint16_t val);
 
 //CV-IN Polling interval
 #define POLL_ZYNAPTIK_CVINS_US 40000
-
+pthread_mutex_t zynaptik_cvin_lock;
 pthread_t init_poll_zynaptik_cvins();
+
+//-----------------------------------------------------------------------------
+// CV-OUT: Set Analog Outputs from MIDI: CC, Notes (velocity+pitchbend)
+//-----------------------------------------------------------------------------
+
+#define MAX_NUM_ZYNCVOUTS 4
+#define DEFAULT_K_CVOUT 1.0
+
+struct zyncvout_st {
+	uint8_t enabled;
+
+	int midi_evt;
+	uint8_t midi_chan;
+	uint8_t midi_num;
+
+	uint16_t midi_event_temp;
+	uint16_t midi_event_mask;
+
+	uint16_t val;
+};
+struct zyncvout_st zyncvouts[MAX_NUM_ZYNCVOUTS];
+
+float k_cvout;
+void set_k_cvout(float k);
+float get_k_cvout();
+
+void setup_zynaptik_cvout(uint8_t i, int midi_evt, uint8_t midi_chan, uint8_t midi_num);
+void disable_zynaptik_cvout(uint8_t i);
+void zynaptik_midi_to_cvout(jack_midi_event_t *ev);
+void set_zynaptik_cvout(int i, uint16_t val);
+void refresh_zynaptik_cvouts();
+
+//CV-OUT Refresh interval
+#define REFRESH_ZYNAPTIK_CVOUTS_US 40000
+
+//-----------------------------------------------------------------------------
+// GATE-OUT: Set Digital Outputs from MIDI Notes
+//-----------------------------------------------------------------------------
+
+#define MAX_NUM_ZYNGATEOUTS 24
+
+struct zyngateout_st {
+	uint8_t enabled;
+
+	int midi_evt;
+	uint8_t midi_chan;
+	uint8_t midi_num;
+
+	uint16_t midi_event_temp;
+	uint16_t midi_event_mask;
+};
+struct zyngateout_st zyngateouts[MAX_NUM_ZYNGATEOUTS];
+
+void setup_zynaptik_gateout(uint8_t i, int midi_evt, uint8_t midi_chan, uint8_t midi_num);
+void disable_zynaptik_gateout(uint8_t i);
+void zynaptik_midi_to_gateout(jack_midi_event_t *ev);
 
 //-----------------------------------------------------------------------------
 // Zynaptik Library Initialization
